9f8f5136f2e96eaddf8b5a7800dc86ce489c2c13
NorbiPeti <szatmari.norbert.peter@gmail.com>
2018-05-28 18:25:12 +0200

    Fix location

--- include.js
+++ include.js
@@ -1,2 +1,2 @@
-const domain = window.location.hostname === "tbmcplugins.github.io" ? "server.figytuna.com:8080" : "localhost:8080";
+const domain = window.location.hostname !== "localhost" ? "server.figytuna.com" : "localhost:8080";
 console.log("Backend: " + domain);
